70bda3c9efbb270bd711ff1f1ec056e94249ef90
Braulio <brauliocassule94@gmail.com>
2017-12-10 16:33:15 +0100

    added firebase auth dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'dart:async';
+
+final FirebaseAuth _auth = FirebaseAuth.instance;
 
 void main() => runApp(
   new Center(
