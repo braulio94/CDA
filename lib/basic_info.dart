59671b9d748eb4ff5728329a870b3ccdaffc962f
Braulio Cassule <brauliocassule94@gmail.com>
2018-03-31 19:10:33 +0100

    member basic info

--- lib/basic_info.dart
+++ lib/basic_info.dart
@@ -14,26 +14,36 @@ class MemberBasicInfo extends StatelessWidget {
       child: new Card(
         child: new Container(
           height: 200.0,
-          child: new Container(
-              alignment: Alignment.topRight,
-              margin: const EdgeInsets.only(top: 20.0, right: 15.0),
-              child: new Column(
-                children: <Widget>[
-                  new Text(
-                      '${member.name}',
-                      textAlign: TextAlign.right,
-                      textDirection: TextDirection.rtl,
-                      style: const TextStyle(fontSize: 20.0, color: Colors.black, fontFamily: 'Mina')
-                  ),
-                  new Text(
-                      '${member.email}',
-                      textAlign: TextAlign.right,
-                      textDirection: TextDirection.rtl,
-                      style: const TextStyle(fontSize: 12.0, color: Colors.black, fontFamily: 'Mina')
-                  ),
-                ],
+          child: new Stack(
+            children: <Widget>[
+              new Container(
+                alignment: Alignment.topRight,
+                margin: const EdgeInsets.only(top: 20.0, right: 15.0),
+                child: new Column(
+                  children: <Widget>[
+                    new Text(
+                        '${member.name}',
+                        textAlign: TextAlign.right,
+                        textDirection: TextDirection.rtl,
+                        style: const TextStyle(fontSize: 20.0, color: Colors.black, fontFamily: 'Mina')
+                    ),
+                    new Text(
+                        '${member.email}',
+                        textAlign: TextAlign.right,
+                        textDirection: TextDirection.rtl,
+                        style: const TextStyle(fontSize: 12.0, color: Colors.black, fontFamily: 'Mina')
+                    ),
+                  ],
+                ),
               ),
-          ),
+              new Container(
+                height: 0.5,
+                width: double.INFINITY,
+                color: Colors.grey[800],
+                margin: const EdgeInsets.only(top: 80.0),
+              )
+            ],
+          )
         ),
       ),
     );
